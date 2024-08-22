43d4bfe3b3f03f826cdfec4964bce34b45a02168
lauralimachi <erickalaura133@gmail.com>
2024-08-22 00:30:44 -0400

    first commit

---
+++ src/screens/Default/_components/GitHub.jsx
@@ -0,0 +1,16 @@
+const Content = () => {
+    return (
+        <div>
+            <a
+                className="App-link"
+                href="https://github.com/lauralimachi/desarrollo-fontend-react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Mi proyecto react (GitHub)
+            </a>
+        </div>
+    );
+};
+
+export default Content;
